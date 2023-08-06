0d8c3245c6456a159e296e4b2930ca0a806fbe95
Kataiser <mecharon1.gm@gmail.com>
2023-08-06 16:00:09 -0500

    Prod fixes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import datetime
+import enum
 import io
 import os
 import re
 import sys
+import time
 import traceback
 
 import discord
@@ -13,13 +15,15 @@ import ujson
 intents = discord.Intents.none()
 intents.message_content = True
 client = discord.Client(intents=intents)
+modes = enum.Enum('Modes', ['scraping', 'downloading', 'posting'])
+MODE = modes.posting
 
 
 def main():
     with open('secret.txt', 'r') as secret_txt:
         secret = secret_txt.read()
 
-    requests_cache.install_cache('dl_cache')
+    requests_cache.install_cache(r'E:\strats_dl_cache.sqlite')
     client.run(secret, log_handler=None)
 
 
@@ -28,19 +32,30 @@ async def on_ready():
     print(f"Logged in as {client.user}\n")
     test_channel_id = 1137437338000162938
     real_channel_id = 617809769322774533
-    channel = await client.fetch_channel(test_channel_id)
+    mod_channel_id = 923103459354480691
+    channel = await client.fetch_channel(real_channel_id)
+    messages_dir = f'messages_{channel.id}'
 
-    if SCRAPING:
-        await scrape(channel)
-    else:
-        await post(channel)
+    if not os.path.isdir(messages_dir):
+        os.mkdir(messages_dir)
+        time.sleep(0.2)
+
+    match MODE:
+        case modes.scraping:
+            await scrape(channel, messages_dir)
+        case modes.downloading:
+            await download(messages_dir)
+        case modes.posting:
+            await post(channel, messages_dir)
+        case _:
+            print("Invalid mode")
 
     print("\nDONE")
     await client.close()
 
 
-async def scrape(channel: discord.TextChannel):
-    with open('oldest_scraped.txt', 'r+') as oldest_scraped_file:
+async def scrape(channel: discord.TextChannel, messages_dir: str):
+    with open(f'oldest_scraped_{channel.id}.txt', 'r+') as oldest_scraped_file:
         oldest_scraped_read = oldest_scraped_file.read()
 
         if oldest_scraped_read:
@@ -49,10 +64,13 @@ async def scrape(channel: discord.TextChannel):
         else:
             oldest_scraped = datetime.datetime.now()
 
-        async for message in channel.history(limit=None, oldest_first=True, before=oldest_scraped):
+        async for message in channel.history(limit=None, oldest_first=True, after=oldest_scraped):
             gfycat_url_result = re_gfycat_url.search(message.content)
 
             if not gfycat_url_result:
+                if 'gfycat' in message.content:
+                    print(f"WARNING: Schrödinger's gfycat: {message.jump_url}")
+
                 continue
 
             message_timestamp = int(message.created_at.replace(tzinfo=datetime.timezone.utc).timestamp())
@@ -64,44 +82,60 @@ async def scrape(channel: discord.TextChannel):
             if not tags:
                 print("WARNING: no tags")
 
-            with open(f'messages\\{message.id}.json', 'w', encoding='UTF8') as message_file:
-                ujson.dump(message_data, message_file, indent=4, escape_forward_slashes=False)
+            with open(f'{messages_dir}\\{message.id}.json', 'w', encoding='UTF8') as message_file:
+                ujson.dump(message_data, message_file, indent=4, escape_forward_slashes=False, ensure_ascii=False)
 
             oldest_scraped_file.seek(0)
             oldest_scraped_file.truncate()
             oldest_scraped_file.write(str(message_timestamp))
+            oldest_scraped_file.flush()
 
 
-async def post(channel: discord.TextChannel):
-    with open('posted.txt', 'r+') as posted_file:
+async def download(messages_dir: str):
+    for message_filename in os.listdir(messages_dir):
+        with open(f'{messages_dir}\\{message_filename}', 'r') as message_file:
+            message_data = ujson.load(message_file)
+
+        print(message_data)
+        video_data, video_filename = download_from_gfycat_url(message_data['gfycat_url'])
+        print(len(video_data), video_filename)
+
+
+async def post(channel: discord.TextChannel, messages_dir: str):
+    with open(f'posted_{channel.id}.txt', 'r+') as posted_file:
         posted_messages = posted_file.read()
 
-        for message_filename in os.listdir('messages'):
+        for message_filename in os.listdir(messages_dir):
             if message_filename in posted_messages:
                 print(f"Skipping {message_filename}")
                 continue
 
-            with open(f'messages\\{message_filename}', 'r') as message_file:
+            with open(f'{messages_dir}\\{message_filename}', 'r', encoding='UTF8') as message_file:
                 message_data = ujson.load(message_file)
 
             print(message_data)
-            gif_html = requests.get(message_data['gfycat_url'], timeout=10).text
-            video_url = re_video_url.search(gif_html).group()
-            video_file = discord.File(io.BytesIO(requests.get(video_url, timeout=20).content), filename=video_url.rpartition('/')[2])
+            thread = await channel.create_thread(name=f"{message_data['tags']} from {message_data['author_name']}", type=discord.ChannelType.public_thread)
+            video_data, video_filename = download_from_gfycat_url(message_data['gfycat_url'])
+            video_file = discord.File(io.BytesIO(video_data), filename=video_filename)
             thread_message = (f"Strat by: <@{message_data['author_id']}>"
                               f"\nPosted on: <t:{message_data['time']}:f>"
                               f"\nOriginal message: {message_data['link']}"
                               f"\n\n{message_data['content']}")
-            thread = await channel.create_thread(name=f"{message_data['tags']} from {message_data['author_name']}", type=discord.ChannelType.public_thread)
             await thread.send(thread_message, file=video_file, suppress_embeds=True, allowed_mentions=discord.AllowedMentions(users=False))
             await thread.edit(archived=True)
 
             async for message in channel.history(limit=3):
                 if message.author == client.user:
                     await message.delete()
-                    break
 
             posted_file.write(f'{message_filename}\n')
+            posted_file.flush()
+
+
+def download_from_gfycat_url(url: str) -> tuple[bytes, str]:
+    gif_html = requests.get(url, timeout=10).text
+    video_url = re_video_url.search(gif_html).group()
+    return requests.get(video_url, timeout=20).content, video_url.rpartition('/')[2]
 
 
 @client.event
@@ -109,7 +143,6 @@ async def on_error(*args):
     print(traceback.format_exc(), file=sys.stderr)
 
 
-SCRAPING = False
 re_gfycat_url = re.compile(r'https://gfycat\.com/[A-Za-z]+')
 re_video_url = re.compile(r'https://giant\.gfycat\.com/[A-Za-z]+\.mp4')
 
